8000b0b339a529eccfb479137f96d3fe44565137
yumeyuniko <daigocafe@msn.com>
2022-03-11 00:58:29 +0900

    first commit

---
+++ app.js
@@ -0,0 +1,24 @@
+require('dotenv').config();
+
+const express = require('express');
+const app = express();
+const taskRoute = require('./routes/tasks');
+const connectDB = require('./db/connect');
+
+app.use(express.json());
+
+const PORT = 5005;
+//Routing
+app.use('/api/v1/tasks', taskRoute);
+
+//DB connect
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGO_URL);
+    app.listen(PORT, console.log('サーバーが起動しました'));
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+start();
